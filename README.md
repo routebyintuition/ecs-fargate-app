797804e67909acc4f9b3e2c9ff9435ec81223af9
Fontes <fontesj@186590cd9477.ant.amazon.com>
2019-05-08 12:48:34 -0700

    Fargate app

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # ecs-fargate-app
+
 An AWS Elastic Container Service Fargate example application with Go and Cloudformation using Redis ElastiCache for storage.
+
+## Usage
+
+You will need a docker image stored in a repository in order to get started. I typically use AWS ECS but any other repository will work so long as you have
+sufficient permissions to download the image to the Fargate cluster.
+
+To build and deploy the demo web application included in the webapp/ directory, follow the instructions in the Demo webapp segment.
+
+
+
+## Demo webapp
+
+There is a web application included which provides a demo site for you to test your deployment.
+
+## AWS CloudFormation
+
+You can use the provided cloudformation template to deploy the needed infrastructure for AWS RDS, ElastiCache, ELB/ALB, and ECS.
+
--- webapp/main.go
+++ webapp/main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"encoding/base64"
+	"flag"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"os"
 	"strconv"
@@ -14,18 +15,17 @@ type serviceConfig struct {
 	RedisHost string
 	RedisPort string
 	AppPort   int
+	Website   []byte
 	RedisConn *redis.Pool
 }
 
 func (sc *serviceConfig) homeHandler(w http.ResponseWriter, r *http.Request) {
-	outputEncoded := "ICAgICAgICAgICAgLy8gICAgICAgICAgIC8vCiAgICAgICAgICAgLy8vICAgICAgICAgIC8vLwogICAgICAgICAgLy8vLyAgICAgICAgIC8vLy8KICAgICAgICAgIHwvLy8vICAgICAgIC8vLy8vCiAgICAgICAgICB8KSkvLzsgICAgIC8pKSkvLzsKICAgICAgICAgLykpKSkpLzsgICAvKSkpKSkvOwogICAgIC4tLS1gLCkpKSkvOyAgLykpKSkpKSkvOwogX18tLVwvNi0gIFxgKSkvOyB8KSkpKSkpKS87CigtLS0tLyAgICBcXFxgYDsgIHwpKSkpKSkvOwogICB+Ly1cICBcXFxcXGBgICAgXCkpKSkpKS87CiAgICAgICBcXFxcXFxcXGAgICAgfCkpKSkpLzsKICAgICAgIHxcXFxcXFxcXF9fXy8pKSkpKSkvO19fLS0tLS0tLS4KICAgICAgIC8vLy8vL3wgICUlXy8pKSkpKSkvOyAgICAgICAgICAgXF9fXywKICAgICAgfHx8fHx8fFwgICBcJSUlJVZMSzs6ICAgICAgICAgICAgICBcXy4gXAogICAgICB8XFxcXFxcXFxcICAgICAgICAgICAgICAgICAgICAgICAgfCAgfCB8CiAgICAgICBcXFxcXFxcICAgICAgICAgICAgICAgICAgICAgICAgICB8ICB8IHwKICAgICAgICB8XFxcXCAgICAgICAgICAgICAgIF9ffCAgICAgICAgLyAgIC8gLwogICAgICAgIHwgXFxfX1wgICAgIFxfX18tLS0tICB8ICAgICAgIHwgICAvIC8KICAgICAgICB8ICAgIC8gfCAgICAgPiAgICAgXCAgIFwgICAgICBcICAvIC8KICAgICAgICB8ICAgLyAgfCAgICAvICAgICAgIFwgICBcICAgICAgPi8gLyAgLCwKICAgICAgICB8ICAgfCAgfCAgIHwgICAgICAgICB8ICAgfCAgICAvLyAvICAvLywKICAgICAgICB8ICAgfCAgfCAgIHwgICAgICAgICB8ICAgfCAgIC98IHwgICB8XFwsCiAgICAgXy0tJyAgIF8tLScgICB8ICAgICBfLS0tXy0tLScgIHwgIFwgXF9fL1x8LwogICAgKC0oLT09PSgtKC0oPT09LyAgICAoLSgtPSgtKC0oPT0vICAgXF9fX18vCiAgICAgICAgICAgICAgICAgICAtVmFsa3lyaWUtCgoK"
-	outputText, err := base64.StdEncoding.DecodeString(outputEncoded)
+	//outputEncoded := "ICAgICAgICAgICAgLy8gICAgICAgICAgIC8vCiAgICAgICAgICAgLy8vICAgICAgICAgIC8vLwogICAgICAgICAgLy8vLyAgICAgICAgIC8vLy8KICAgICAgICAgIHwvLy8vICAgICAgIC8vLy8vCiAgICAgICAgICB8KSkvLzsgICAgIC8pKSkvLzsKICAgICAgICAgLykpKSkpLzsgICAvKSkpKSkvOwogICAgIC4tLS1gLCkpKSkvOyAgLykpKSkpKSkvOwogX18tLVwvNi0gIFxgKSkvOyB8KSkpKSkpKS87CigtLS0tLyAgICBcXFxgYDsgIHwpKSkpKSkvOwogICB+Ly1cICBcXFxcXGBgICAgXCkpKSkpKS87CiAgICAgICBcXFxcXFxcXGAgICAgfCkpKSkpLzsKICAgICAgIHxcXFxcXFxcXF9fXy8pKSkpKSkvO19fLS0tLS0tLS4KICAgICAgIC8vLy8vL3wgICUlXy8pKSkpKSkvOyAgICAgICAgICAgXF9fXywKICAgICAgfHx8fHx8fFwgICBcJSUlJVZMSzs6ICAgICAgICAgICAgICBcXy4gXAogICAgICB8XFxcXFxcXFxcICAgICAgICAgICAgICAgICAgICAgICAgfCAgfCB8CiAgICAgICBcXFxcXFxcICAgICAgICAgICAgICAgICAgICAgICAgICB8ICB8IHwKICAgICAgICB8XFxcXCAgICAgICAgICAgICAgIF9ffCAgICAgICAgLyAgIC8gLwogICAgICAgIHwgXFxfX1wgICAgIFxfX18tLS0tICB8ICAgICAgIHwgICAvIC8KICAgICAgICB8ICAgIC8gfCAgICAgPiAgICAgXCAgIFwgICAgICBcICAvIC8KICAgICAgICB8ICAgLyAgfCAgICAvICAgICAgIFwgICBcICAgICAgPi8gLyAgLCwKICAgICAgICB8ICAgfCAgfCAgIHwgICAgICAgICB8ICAgfCAgICAvLyAvICAvLywKICAgICAgICB8ICAgfCAgfCAgIHwgICAgICAgICB8ICAgfCAgIC98IHwgICB8XFwsCiAgICAgXy0tJyAgIF8tLScgICB8ICAgICBfLS0tXy0tLScgIHwgIFwgXF9fL1x8LwogICAgKC0oLT09PSgtKC0oPT09LyAgICAoLSgtPSgtKC0oPT0vICAgXF9fX18vCiAgICAgICAgICAgICAgICAgICAtVmFsa3lyaWUtCgoK"
+	//outputText, err := base64.StdEncoding.DecodeString(string(sc.Website))
 	count := sc.incrementCounter()
 	countString := []byte("\nVisit Counter: " + strconv.Itoa(count) + "\n")
-	outputText = append(outputText[:], countString[:]...)
-	if err != nil {
-		fmt.Println("Could not parse text: ", err)
-	}
+	outputText := append(sc.Website[:], countString[:]...)
+
 	fmt.Fprintf(w, string(outputText))
 }
 
@@ -36,12 +36,22 @@ func (sc *serviceConfig) incrementCounter() int {
 	return count
 }
 
+var port = flag.Int("port", 80, "Port to run webserver on")
+
 func main() {
 	logInit()
+
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
+		flag.PrintDefaults()
+	}
+
+	flag.Parse()
+
 	var sc serviceConfig
 	sc.RedisHost = os.Getenv("RedisEndpoint")
 	sc.RedisPort = os.Getenv("RedisPort")
-	sc.AppPort = 8089
+	sc.AppPort = *port
 	sc.dbInit()
 	defer sc.RedisConn.Close()
 
@@ -54,6 +64,15 @@ func main() {
 		Error.Println("No Redis port provided...exiting...")
 		os.Exit(1)
 	}
+
+	art, err := ioutil.ReadFile("art.txt")
+
+	if err != nil {
+		Error.Println("Could not open art.txt")
+	}
+
+	copy(sc.Website, art)
+	fmt.Println("Art: ", art)
 
 	http.HandleFunc("/", sc.homeHandler)
 
